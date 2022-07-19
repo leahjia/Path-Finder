9c293abd907f8cb3e76a31afabf5d448707295cd
Leah Jia <yuwenj3@uw.edu>
2022-07-18 18:36:16 -0700

    class & methods update

--- hw-graph/src/main/java/graph/UnivMap.java
+++ hw-graph/src/main/java/graph/UnivMap.java
@@ -5,98 +5,109 @@ import org.w3c.dom.Node;
 import java.util.Map;
 
 /**
- * This class represents the mathematical concept of a line segment. Think of
- * this as a pair (start-point, end-point), containing the starting and ending
- * points of the line.
+ * This class represents the UW campus map, containing information such as the names
+ * of the buildings and the distance from one building to another.
+ * This class can be a tool to find the connections among buildings and available
+ * paths between two buildings.
  *
  * Abstract Invariant:
- *  A line's start-point must be different from its end-point.
+ *  To find a path, the starting building must be different from the destination.
  */
 public class UnivMap {
 
     private Map UnivMap;
 
     /**
-     * (What it does)
-     * @param
-     * @spec.requires
+     * Adds a new building to the university map
+     * @param A Node representing the building to be added to the map
+     * @spec.requires A != null
      * @spec.modifies this.UnivMap
-     * @spec.effects
+     * @spec.effects Adds a new Node A to UnivMap
      */
     public void AddNode(Node A) {
     }
 
-
     /**
-     * Removes a node to the university map
+     * Removes an existing building from the university map
      * @param A Node representing building to be removed from university map
      * @spec.requires A != null && UnivMap.contains(A)
      * @spec.modifies this.UnivMap
-     * @spec.effects Removes the Node A from UniMap
+     * @spec.effects Removes the Node A from UnivMap
      */
     public void RemoveNode(Node A) {
         // Note: all the edges from and to A will be removed as well
     }
 
     /**
-     * Adds a new node to the university map
-     * @param A Node representing a building to be added to the university map
-     * @spec.requires A != null
+     * Adds a new route from building A to B on the university map
+     * @param A Node representing building A on the university map
+     * @param B Node representing building B on the university map
+     * @param dist weight on the Edge to be added to the university map,
+     *             representing the distance of that route between A and B
+     * @spec.requires A != null && B != null && !A.equals(B) && dist > 0
      * @spec.modifies this.UnivMap
-     * @spec.effects Adds a new node A to UniMap
+     * @spec.effects Adds a new Edge from A to B to UnivMap
      */
     public void AddEdge(Node A, Node B, int dist) {
     }
 
     /**
-     * (What it does)
-     * @param
-     * @spec.requires
+     * Removes the route from building A to B from the university map
+     * @param A Node representing building A on the university map
+     * @param B Node representing building B on the university map
+     * @spec.requires A != null && B != null && UnivMap.contains(A) && UnivMap.contains(A)
      * @spec.modifies this.UnivMap
-     * @spec.effects
+     * @spec.effects Removes the Edge from A to B from UnivMap
      */
     public void RemoveEdge(Node A, Node B) {
     }
 
     /**
-     * (What it does)
-     * @param
-     * @spec.requires
-     * @spec.modifies this.UnivMap
-     * @spec.effects
+     * Checks if building A exists on the map
+     * @param A Node representing the building A to be checked on the map
+     * @return a boolean that will be true if A exists and false otherwise
+     * @spec.requires A != null
      */
     public void contains(Node A) {
+        // include the case when A is null
     }
 
     /**
-     * (What it does)
-     * @param
-     * @spec.requires
-     * @spec.modifies this.UnivMap
-     * @spec.effects
+     * Lists all the buildings that can be reached starting from building A
+     * @param A the Node representing the starting building
+     * @return a series of 
+     * @spec.requires A != null && UnivMap.contains(A)
      */
     public void ListChildren(Node A) {
     }
 
     /**
-     * (What it does)
-     * @param
-     * @spec.requires
-     * @spec.modifies this.UnivMap
-     * @spec.effects
+     * Lists all the parent Nodes of A
+     * @param A the Node we want to find the parents of
+     * @return All the Nodes that are parent(s) of Node A
+     * @spec.requires UnivMap.contains(A)
      */
     public void ListParents(Node A) {
     }
 
     /**
-     * (What it does)
-     * @param
-     * @spec.requires
-     * @spec.modifies this.UnivMap
-     * @spec.effects
+     * Finds an available path that can be taken from building A to building B
+     * @param A the source, or starting point
+     * @param B the destination
+     * @return a series of Edges from A to B
+     * @spec.requires UnivMap.contains(A) && UnivMap.contains(B) && !A.equals(B)
      */
     public void FindPath(Node A, Node B) {
     }
 
+    /**
+     * Finds the distance from building A to building B
+     * @param A the source, or starting point
+     * @param B the destination
+     * @return an integer representing the distance from A to B
+     * @spec.requires UnivMap.contains(A) && UnivMap.contains(B) && !A.equals(B)
+     */
+    public void FindDistance(Node A, Node B) {
+    }
 
 }
