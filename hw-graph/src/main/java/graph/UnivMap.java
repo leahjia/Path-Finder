4588a3cff199635222778c7be67e7624c6e1833a
Leah Jia <yuwenj3@uw.edu>
2022-07-19 17:30:33 -0700

    arranging

--- hw-graph/src/main/java/graph/UnivMap.java
+++ hw-graph/src/main/java/graph/UnivMap.java
@@ -1,9 +1,8 @@
 package graph;
 
 import org.w3c.dom.Node;
-import java.util.ArrayList;
 import java.util.List;
-import graph.*;
+//import graph.*;
 
 /**
  * This class represents the UW campus map, containing information such as the names
@@ -18,11 +17,6 @@ public class UnivMap {
 
     // fields, abstraction function, rep invariant , fields, methods, etc
 
-    // Abstraction Function:
-    // AF(this) =
-
-    // Rep Invariant:
-
     /**
      * Adds a new building to the university map
      * @param A Node representing the building to be added to the map
@@ -31,6 +25,7 @@ public class UnivMap {
      * @spec.effects Adds a new Node A to UnivMap
      */
     public void AddNode(Node A) {
+        throw new RuntimeException("Not implemented yet.");
     }
 
     /**
@@ -41,19 +36,20 @@ public class UnivMap {
      * @spec.effects Removes the Node A from UnivMap
      */
     public void RemoveNode(Node A) {
+        throw new RuntimeException("Not implemented yet.");
     }
 
     /**
      * Adds a new route from building A to B on the university map
      * @param A Node representing building A on the university map
      * @param B Node representing building B on the university map
-     * @param dist weight on the Edge to be added to the university map,
-     *             representing the distance of that route between A and B
+     * @param str label of the Edge from A to B to be added to the university map,
      * @spec.requires A != null, B != null, !A.equals(B), and dist is greater than 0
      * @spec.modifies this.UnivMap
      * @spec.effects Adds a new Edge from A to B to UnivMap
      */
-    public void AddEdge(Node A, Node B, int dist) {
+    public void AddEdge(Node A, Node B, String str) {
+        throw new RuntimeException("Not implemented yet.");
     }
 
     /**
@@ -65,6 +61,7 @@ public class UnivMap {
      * @spec.effects Removes the Edge from A to B from UnivMap
      */
     public void RemoveEdge(Node A, Node B) {
+        throw new RuntimeException("Not implemented yet.");
     }
 
     /**
@@ -74,7 +71,7 @@ public class UnivMap {
      * @spec.requires A != null
      */
     public boolean contains(Node A) {
-        return true; // implement in hw6
+        throw new RuntimeException("Not implemented yet.");
     }
 
     /**
@@ -84,7 +81,7 @@ public class UnivMap {
      * @spec.requires A != null and UnivMap.contains(A)
      */
     public List ListChildren(Node A) {
-        return new ArrayList(); // implement this in hw6
+        throw new RuntimeException("Not implemented yet.");
     }
 
     /**
@@ -94,7 +91,7 @@ public class UnivMap {
      * @spec.requires UnivMap.contains(A)
      */
     public List ListParents(Node A) {
-        return new ArrayList(); // implement this in hw6
+        throw new RuntimeException("Not implemented yet.");
     }
 
     /**
@@ -105,18 +102,18 @@ public class UnivMap {
      * @spec.requires UnivMap.contains(A), UnivMap.contains(B), and !A.equals(B)
      */
     public List FindPath(Node A, Node B) {
-        return new ArrayList(); // implement this in hw6
+        throw new RuntimeException("Not implemented yet.");
     }
 
-    /**
-     * Finds the distance from building A to building B
-     * @param A the source, or starting point
-     * @param B the destination
-     * @return an integer representing the distance from A to B
-     * @spec.requires UnivMap.contains(A), UnivMap.contains(B), and !A.equals(B)
-     */
-    public int FindDistance(Node A, Node B) {
-        return -1; // implement this in hw6
-    }
+//    /**
+//     * Finds the distance from building A to building B
+//     * @param A the source, or starting point
+//     * @param B the destination
+//     * @return an integer representing the distance from A to B
+//     * @spec.requires UnivMap.contains(A), UnivMap.contains(B), and !A.equals(B)
+//     */
+//    public int FindDistance(Node A, Node B) {
+//        throw new RuntimeException("Not implemented yet.");
+//    }
 
 }
