d1e75ed02c1c88aaca0c0d0caf1769117bd84698
Leah Jia <yuwenj3@uw.edu>
2022-07-21 13:31:55 -0700

    arranging

--- hw-graph/src/main/java/graph/UnivMap.java
+++ hw-graph/src/main/java/graph/UnivMap.java
@@ -11,7 +11,8 @@ import java.util.List;
  * paths between two buildings.
  *
  * Abstract Invariant:
- *  To find a path, the starting building must be different from the destination.
+ *  Each Node and each edge needs to be unique.
+ *  An edge's source must be different from its destination.
  */
 public class UnivMap {
 
@@ -21,7 +22,7 @@ public class UnivMap {
      * Adds a new building to the university map
      * @param A Node representing the building to be added to the map
      * @spec.requires A != null
-     * @spec.modifies this.UnivMap
+     * @spec.modifies this
      * @spec.effects Adds a new Node A to UnivMap
      */
     public void AddNode(Node A) {
@@ -32,7 +33,7 @@ public class UnivMap {
      * Removes an existing building from the university map
      * @param A Node representing building to be removed from university map
      * @spec.requires A != null and UnivMap.contains(A)
-     * @spec.modifies this.UnivMap
+     * @spec.modifies this
      * @spec.effects Removes the Node A from UnivMap
      */
     public void RemoveNode(Node A) {
@@ -41,26 +42,28 @@ public class UnivMap {
 
     /**
      * Adds a new route from building A to B on the university map
-     * @param A Node representing building A on the university map
-     * @param B Node representing building B on the university map
-     * @param str label of the Edge from A to B to be added to the university map,
-     * @spec.requires A != null, B != null, !A.equals(B), and dist is greater than 0
+     * @param source the source of the new edge
+     * @param destination the destination of this new edge
+     * @param label label of the Edge from A to B to be added to the university map
+     * @spec.requires source != null, destination != null, !source.equals(destination),
+     *                and !UnivMap.contains(label)
      * @spec.modifies this.UnivMap
-     * @spec.effects Adds a new Edge from A to B to UnivMap
+     * @spec.effects Adds a new Edge from A to B to university map
      */
-    public void AddEdge(Node A, Node B, String str) {
+    public void AddEdge(Node source, Node destination, String label) {
         throw new RuntimeException("Not implemented yet.");
     }
 
     /**
      * Removes the route from building A to B from the university map
-     * @param A Node representing building A on the university map
-     * @param B Node representing building B on the university map
-     * @spec.requires A != null, B != null, UnivMap.contains(A), and UnivMap.contains(A)
+     * @param source Node representing the source of the edge
+     * @param destination Node representing the destination of the edge
+     * @spec.requires source != null, destination != null,
+     *                UnivMap.contains(source), and UnivMap.contains(destination)
      * @spec.modifies this.UnivMap
      * @spec.effects Removes the Edge from A to B from UnivMap
      */
-    public void RemoveEdge(Node A, Node B) {
+    public void RemoveEdge(Node source, Node destination) {
         throw new RuntimeException("Not implemented yet.");
     }
 
@@ -93,27 +96,5 @@ public class UnivMap {
     public List ListParents(Node A) {
         throw new RuntimeException("Not implemented yet.");
     }
-
-    /**
-     * Finds an available path that can be taken from building A to building B
-     * @param A the source, or starting point
-     * @param B the destination
-     * @return a List of Edges from A to B
-     * @spec.requires UnivMap.contains(A), UnivMap.contains(B), and !A.equals(B)
-     */
-    public List FindPath(Node A, Node B) {
-        throw new RuntimeException("Not implemented yet.");
-    }
-
-//    /**
-//     * Finds the distance from building A to building B
-//     * @param A the source, or starting point
-//     * @param B the destination
-//     * @return an integer representing the distance from A to B
-//     * @spec.requires UnivMap.contains(A), UnivMap.contains(B), and !A.equals(B)
-//     */
-//    public int FindDistance(Node A, Node B) {
-//        throw new RuntimeException("Not implemented yet.");
-//    }
 
 }
