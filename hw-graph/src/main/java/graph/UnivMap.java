3d08cbadbda0fb41fdb1795b0fec81c7935685a5
Leah Jia <yuwenj3@uw.edu>
2022-07-21 22:05:36 -0700

    update

--- hw-graph/src/main/java/graph/UnivMap.java
+++ hw-graph/src/main/java/graph/UnivMap.java
@@ -1,8 +1,7 @@
 package graph;
 
-import org.w3c.dom.Node;
 import java.util.List;
-//import graph.*;
+import org.w3c.dom.Node;
 
 /**
  * UnivMap is a mutable representation of the UW campus map with nodes and edges.
@@ -21,30 +20,31 @@ public class UnivMap {
      * @spec.modifies this
      * @spec.effects Creates a new map
      */
-    public void CreateMap() {
+    public UnivMap() {
 
     }
 
     /**
      * Adds a new building to the campus map
      * @param A Node representing the building to be added to the map
-     * @throws IllegalArgumentException if the same node already exist
+     * @throws IllegalArgumentException if the same node already exist or A is null
      * @spec.requires A != null and !UnivMap.contains(A)
      * @spec.modifies this
      * @spec.effects Adds a new Node A to UnivMap
      */
-    public void AddNode(Node A) {
+    public void AddNode(String A) {
         throw new RuntimeException("Not implemented yet.");
     }
 
     /**
      * Removes an existing building from the campus map
      * @param A Node representing building to be removed from campus map
+     * @throws IllegalArgumentException if A is null or !UnivMap.contains(A)
      * @spec.requires A != null and UnivMap.contains(A)
      * @spec.modifies this
      * @spec.effects Removes the Node A from UnivMap
      */
-    public void RemoveNode(Node A) {
+    public void RemoveNode(String A) {
         throw new RuntimeException("Not implemented yet.");
     }
 
@@ -53,55 +53,61 @@ public class UnivMap {
      * @param source the source of the new edge
      * @param destination the destination of this new edge
      * @param label label of the Edge from A to B to be added to the campus map
-     * @spec.requires source != null, destination != null, !source.equals(destination),
-     *                and !UnivMap.contains(label)
+     * @throws IllegalArgumentException if source is null, destination is null,
+     *         label != null, source.equals(destination), or UnivMap.contains(label)
+     * @spec.requires source != null, destination != null, label != null,
+     *                !source.equals(destination), and !UnivMap.contains(label)
      * @spec.modifies this.UnivMap
      * @spec.effects Adds a new Edge from A to B to campus map
      */
-    public void AddEdge(Node source, Node destination, String label) {
+    public void AddEdge(String source, String destination, String label) {
         throw new RuntimeException("Not implemented yet.");
     }
 
     /**
      * Removes the route from building A to B from the campus map
-     * @param source Node representing the source of the edge
-     * @param destination Node representing the destination of the edge
+     * @param label the edge representing the connection from source to destination
+     * @throws IllegalArgumentException if source is null, destination is null,
+     *         or source.equals(destination)
      * @spec.requires source != null, destination != null,
      *                UnivMap.contains(source), and UnivMap.contains(destination)
      * @spec.modifies this.UnivMap
      * @spec.effects Removes the Edge from A to B from UnivMap
      */
-    public void RemoveEdge(Node source, Node destination) {
+    public void RemoveEdge(String label) {
         throw new RuntimeException("Not implemented yet.");
     }
 
     /**
      * Checks if building A exists on the map
      * @param A Node representing the building A to be checked on the map
+     * @throws IllegalArgumentException if A is null
      * @return a boolean that will be true if A exists and false otherwise
      * @spec.requires A != null
      */
-    public boolean contains(Node A) {
+    public boolean contains(String A) {
         throw new RuntimeException("Not implemented yet.");
     }
 
     /**
      * Lists all the buildings that can be reached starting from building A
      * @param A the Node representing the starting building
+     * @throws IllegalArgumentException if A is null or !UnivMap.contains(A)
      * @return A List of Nodes that are children of Node A
      * @spec.requires A != null and UnivMap.contains(A)
      */
-    public List ListChildren(Node A) {
+    public List<String> ListChildren(String A) {
         throw new RuntimeException("Not implemented yet.");
     }
 
     /**
      * Lists all the parent Nodes of A
      * @param A the Node we want to find the parents of
+     * @throws IllegalArgumentException if A is null or !UnivMap.contains(A)
      * @return A List of all the Nodes that are parent(s) of Node A
-     * @spec.requires UnivMap.contains(A)
+     * @spec.requires A != null and UnivMap.contains(A)
      */
-    public List ListParents(Node A) {
+    public List<String> ListParents(String A) {
         throw new RuntimeException("Not implemented yet.");
     }
 
--- hw-graph/src/test/java/graph/junitTests/UnivMapTest.java
+++ hw-graph/src/test/java/graph/junitTests/UnivMapTest.java
@@ -2,19 +2,138 @@ package graph.junitTests;
 import graph.*;
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 public class UnivMapTest {
 
-    private final Map<String, UnivMap> graph = new HashMap<String, UnivMap>();
+//    private final UnivMap map1 = new UnivMap();
+    String X = null;
+    String A = "A";
+    String B = "B";
+    String e1 = "e1";
+    String e2 = "e2";
 
-    /** Tests doing something. */
+    /** Tests add and remove node successful. */
     @Test
-    public void testToString() {
-        assertEquals(graph.toString(), "{}");
+    public void testAddNode() {
+        UnivMap map1 = new UnivMap();
+        map1.AddNode(A);
+        map1.AddNode(B);
+        assertTrue(map1.contains(A));
+        assertTrue(map1.contains(B));
+        map1.RemoveNode(A);
+        map1.RemoveNode(B);
+        assertFalse(map1.contains(A));
+        assertFalse(map1.contains(B));
+    }
+
+    /** Tests add and remove edge successful. */
+    @Test
+    public void testAddRemoveEdge() {
+        UnivMap map1 = new UnivMap();
+        map1.AddEdge(A, B, e1);
+        map1.AddEdge(B, A, e2);
+        assertTrue(map1.ListParents(A).contains(B));
+        assertTrue(map1.ListParents(B).contains(A));
+        assertTrue(map1.ListChildren(A).contains(B));
+        assertTrue(map1.ListChildren(B).contains(A));
+        map1.RemoveEdge(e1);
+        map1.RemoveEdge(e2);
+        assertFalse(map1.ListParents(A).contains(B));
+        assertFalse(map1.ListParents(B).contains(A));
+        assertFalse(map1.ListParents(A).contains(B));
+        assertFalse(map1.ListParents(B).contains(A));
+    }
+
+    /** Tests ListChildren and ListParents accuracy. */
+    @Test
+    public void testListChildrenParents() {
+        UnivMap map1 = new UnivMap();
+        map1.AddEdge(A, B, e1);
+        map1.AddEdge(B, A, e2);
+        assertTrue(map1.ListChildren(A).contains(B));
+        assertTrue(map1.ListChildren(B).contains(A));
+        assertTrue(map1.ListParents(A).contains(B));
+        assertTrue(map1.ListParents(B).contains(A));
+    }
+
+    /**
+     * Tests that AddNode throws IllegalArgumentException when adding duplicate node
+     */
+    @Test
+    public void testAddNodeThrowsIllegalArgumentException() {
+        UnivMap map1 = new UnivMap();
+        try {
+            map1.AddNode(X);
+        } catch (IllegalArgumentException e) {
+            fail("Threw IllegalArgumentException for adding null Node: " + e);
+        }
+    }
+
+    /**
+     * Tests that AddEdge throws IllegalArgumentException when adding null values
+     */
+    @Test
+    public void testAddEdgeThrowsIllegalArgumentException() {
+        UnivMap map1 = new UnivMap();
+
+        try {
+            map1.AddEdge(A, B, X);
+        } catch (IllegalArgumentException e) {
+            fail("Threw IllegalArgumentException for adding null label: "
+                    + e);
+        }
+
+        try {
+            map1.AddEdge(A, X, e1);
+        } catch (IllegalArgumentException e) {
+            fail("Threw IllegalArgumentException for adding edge to null destination: "
+                    + e);
+        }
+
+        try {
+            map1.AddEdge(X, B, e1);
+        } catch (IllegalArgumentException e) {
+            fail("Threw IllegalArgumentException for adding edge from null source: "
+                    + e);
+        }
+    }
+
+
+    /**
+     * Tests that RemoveNode, RemoveEdge, ListChildren, and ListParents throws
+     * IllegalArgumentException when asking for elements not in map
+     */
+    @Test
+    public void testRemoveThrowsIllegalArgumentException() {
+        UnivMap map1 = new UnivMap();
+        try {
+            map1.RemoveEdge(e1);
+        } catch (IllegalArgumentException e) {
+            fail("Threw IllegalArgumentException for non-existent edge: "
+                    + e);
+        }
+
+        try {
+            map1.RemoveNode(A);
+        } catch (IllegalArgumentException e) {
+            fail("Threw IllegalArgumentException for non-existent node: "
+                    + e);
+        }
+
+        try {
+            map1.ListChildren(A);
+        } catch (IllegalArgumentException e) {
+            fail("Threw IllegalArgumentException for non-existent node: "
+                    + e);
+        }
+        try {
+            map1.ListParents(A);
+        } catch (IllegalArgumentException e) {
+            fail("Threw IllegalArgumentException for non-existent node: "
+                    + e);
+        }
     }
 
 }
